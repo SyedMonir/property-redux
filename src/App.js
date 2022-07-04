977ee3241bff2d04835c6ea1e45224bd1a965c50
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-07-05 00:11:26 +0600

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
   return (
     <>
       <h1>Hello World</h1>
+      <Dashboard />
     </>
   );
 }
